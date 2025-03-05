28252bae0ec0f6d938d7b31cfad5d3f781b49556
akshey <aksheyvs@gmail.com>
2025-03-05 13:35:59 +0530

    Updating context

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,13 +1,24 @@
 import Header from "./components/layout/Header/Header.tsx";
 import MainComponents from "./components/layout/MainComponents/MainComponents.tsx";
+import { useLocalStorage } from "./hooks/useLocalStorage";
+import { useState } from "react";
+import { Todo } from "./components/layout/MainComponents/InputControl/InputControl.tsx";
+import { TodoListSectionContext } from "./context/TodoListSectionContext";
 
 function App() {
+    const { setItem, getItem } = useLocalStorage("todoArray");
+    const [todoArray, setTodoArray] = useState([]);
+
+    setTodoArray(getItem());
+
     return (
         <div id="root">
             <div className="wrapper light">
                 <div className="container">
                     <Header />
-                    <MainComponents />
+                    <TodoListSectionContext.Provider value={{ todoArray, setItem }}>
+                        <MainComponents />
+                    </TodoListSectionContext.Provider>
                 </div>
             </div>
         </div>
--- todo/src/components/layout/MainComponents/InputControl/InputControl.tsx
+++ todo/src/components/layout/MainComponents/InputControl/InputControl.tsx
@@ -1,20 +1,16 @@
 import { useState } from "react";
-import { useLocalStorage } from "../../../../hooks/useLocalStorage";
 import { v4 as uuidv4 } from "uuid";
-import { TodoListSectionContext } from "../../../../context/TodoListSectionContext";
+import { useTodoListSectionContext } from "../../../../context/TodoListSectionContext";
 
 export type Todo = { key: string; name: string; completed: boolean };
 
 function InputControl() {
-    const { setItem, getItem } = useLocalStorage("todoArray");
     const [inputValue, setInputValue] = useState("");
-    const [todoArray, setTodoArray] = useState();
+    const { todoArray, setItem } = useTodoListSectionContext();
 
     const addTodo = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === "Enter") {
-            let todoArray = getItem();
             todoArray.push({ key: uuidv4(), name: inputValue, completed: false });
-            setTodoArray(todoArray);
             setItem(todoArray);
             setInputValue("");
         }
@@ -24,18 +20,16 @@ function InputControl() {
     };
 
     return (
-        <TodoListSectionContext.Provider value={todoArray}>
-            <input
-                type="text"
-                className="todo-input"
-                value={inputValue}
-                placeholder="Create a new todo..."
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                    setInputValue(e.target.value);
-                }}
-                onKeyDown={addTodo}
-            />
-        </TodoListSectionContext.Provider>
+        <input
+            type="text"
+            className="todo-input"
+            value={inputValue}
+            placeholder="Create a new todo..."
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setInputValue(e.target.value);
+            }}
+            onKeyDown={addTodo}
+        />
     );
 }
 
